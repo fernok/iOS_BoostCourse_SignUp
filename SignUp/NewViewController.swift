c8fa8d9e2b4d36340528373b1b23266698fee01c
fernok <minkim918@gmail.com>
2020-07-22 20:38:20 +0900

    First Commit - Not Complete

---
+++ SignUp/NewViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewViewController.swift
+//  SignUp
+//
+//  Created by Hyuhng Min Kim on 2020/07/13.
+//  Copyright © 2020 Hyuhng Min Kim. All rights reserved.
+//
+
+import UIKit
+
+class NewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ UserInformation.swift
@@ -0,0 +1,9 @@
+//
+//  UserInformation.swift
+//  SignUp
+//
+//  Created by Hyuhng Min Kim on 2020/07/13.
+//  Copyright © 2020 Hyuhng Min Kim. All rights reserved.
+//
+
+import Foundation
